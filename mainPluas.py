911929b4c99bf9240f6b16310f014f439db07eef
Juan Pablo Plúas <jppluas@espol.edu.ec>
2024-01-17 12:04:58 -0500

    modificacion scrapping

--- mainPluas.py
+++ mainPluas.py
@@ -2,17 +2,41 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-linkPopulares='https://steamcharts.com/top'
-preciosPopulares=[]
-caracteristicasPopulares=[]
-esMultijugador=[]
-categoriasPopulares=[]
+linkPopulares = 'https://steamcharts.com/top'
+preciosPopulares = []
+caracteristicasPopulares = []
+esMultijugador = []
+categoriasPopulares = []
+
+
+def agecheckpass():
+  url = 'https://store.steampowered.com/agecheck/app/271590/'
+  response = requests.get(url)
+  html = BeautifulSoup(response.text, 'html.parser')
+
+  form_data = {}
+  for select in html.find_all('select'):
+    select_name = select.get('name')
+    if select_name:
+      form_data[select_name] = select.find('option')['value']
+  form_data['ageDay'] = '1'
+  form_data['ageMonth'] = 'January'
+  form_data['ageYear'] = '1980'
+
+  response = requests.post(url, data=form_data)
+  if response.status_code == 200:
+    print('Formulario enviado con éxito!')
+    return True
+  else:
+    print('Error al enviar el formulario:', response.status_code)
+    print('Contenido de la respuesta:', response.text)
+    return False
 
 
 def scrapGlobal_text(url, hasMany, label, hasClass, labelClass):
-  htmlElement=''
-  htmlElements=[]
-  elements=[]
+  htmlElement = ''
+  htmlElements = []
+  elements = []
   response = requests.get(url)
   if response.status_code == 200:
     html = BeautifulSoup(response.text, 'html.parser')
@@ -39,9 +63,10 @@ def scrapGlobal_text(url, hasMany, label, hasClass, labelClass):
   return elements
 
 
-def scrapSpecific_text(url, hasMany, container, containerClass, label, hasClass, labelClass):
-  htmlElements=[]
-  elements=[]
+def scrapSpecific_text(url, hasMany, container, containerClass, label,
+                       hasClass, labelClass):
+  htmlElements = []
+  elements = []
   response = requests.get(url)
   if response.status_code == 200:
     html = BeautifulSoup(response.text, 'html.parser')
@@ -57,7 +82,7 @@ def scrapSpecific_text(url, hasMany, container, containerClass, label, hasClass,
           #print(htmlElement.text.strip())
       else:
         if hasClass:
-          htmlElement = htmlContainer.find(label, class_= labelClass)
+          htmlElement = htmlContainer.find(label, class_=labelClass)
           #print (htmlElement.text.strip())
           return htmlElement.text.strip()
         else:
@@ -67,57 +92,63 @@ def scrapSpecific_text(url, hasMany, container, containerClass, label, hasClass,
 
     else:
       print(
-          f"No se encontró el contenedor con la clase {containerClass} en la página.")
+          f"No se encontró el contenedor con la clase {containerClass} en la página."
+      )
   else:
     print('Error al acceder a la página:', response.status_code)
   return elements
 
 
-def scrapIds(url, hasMany, container, containerClass, label, hasClass, labelClass):
-  htmlElements=[]
+def srapSpecific_links(url, hasMany, container, containerClass, label,
+                       hasClass, labelClass):
+  htmlElements = []
   links = []
   response = requests.get(url)
   if response.status_code == 200:
-      html = BeautifulSoup(response.text, 'html.parser')
-      htmlContainer = html.find(container, class_=containerClass)
-      if htmlContainer:
-          if hasMany:
-            if hasClass:
-                htmlElements = htmlContainer.find_all(label, class_=labelClass)
-            else:
-                htmlElements = htmlContainer.find_all(label)
-            for htmlElement in htmlElements:
-                link = htmlElement.get('href')
-                if link:
-                    links.append(link.split('/')[-1])
-
-                    #print(link)
-                else:
-                    print('La etiqueta no tiene atributo href.')  
+    html = BeautifulSoup(response.text, 'html.parser')
+    htmlContainer = html.find(container, class_=containerClass)
+    if htmlContainer:
+      if hasMany:
+        if hasClass:
+          htmlElements = htmlContainer.find_all(label, class_=labelClass)
+        else:
+          htmlElements = htmlContainer.find_all(label)
+        for htmlElement in htmlElements:
+          link = htmlElement.get('href')
+          if link:
+            links.append(link)
+            #print(link)
           else:
-            if hasClass:
-              htmlElement = htmlContainer.find(label, class_=labelClass)
-              return htmlElement.get('href').split('/')[-1]
-            else:
-              htmlElement = htmlContainer.find(label)
-              return htmlElement.get('href').split('/')[-1]
-
+            print('La etiqueta no tiene atributo href.')
       else:
-          print(f"No se encontró el contenedor con la clase {containerClass} en la página.")
+        if hasClass:
+          htmlElement = htmlContainer.find(label, class_=labelClass)
+          return htmlElement.get('href')
+        else:
+          htmlElement = htmlContainer.find(label)
+          return htmlElement.get('href')
+
+    else:
+      print(
+          f"No se encontró el contenedor con la clase {containerClass} en la página."
+      )
   else:
-      print('Error al acceder a la página:', response.status_code)
+    print('Error al acceder a la página:', response.status_code)
   return links
 
 
-nombresJuegos=scrapSpecific_text(linkPopulares,True, 'table','common-table','a', False, 'x')
-jugadoresPico=scrapGlobal_text(linkPopulares,True,'td',True, 'peak-concurrent')
-ids=scrapIds(linkPopulares, True, 'table','common-table','a', False, 'x')
-
+nombresJuegos = scrapSpecific_text(linkPopulares, True, 'table',
+                                   'common-table', 'a', False, 'x')
+jugadoresPico = scrapGlobal_text(linkPopulares, True, 'td', True,
+                                 'peak-concurrent')
+ids = srapSpecific_links(linkPopulares, True, 'table', 'common-table', 'a',
+                         False, 'x')
 
 for juego in nombresJuegos:
-  nombre_format= juego.lower().replace(":", "").replace(" ", "+")
-  url='https://gg.deals/games/?title='+nombre_format
-  precio=scrapSpecific_text(url, False, 'span','lowest-recorded','span',True,'price-inner')
+  nombre_format = juego.lower().replace(":", "").replace(" ", "+")
+  url = 'https://gg.deals/games/?title=' + nombre_format
+  precio = scrapSpecific_text(url, False, 'span', 'lowest-recorded', 'span',
+                              True, 'price-inner')
   if precio == 'Free':
     precio = 0
   else:
@@ -125,42 +156,45 @@ for juego in nombresJuegos:
   preciosPopulares.append(precio)
 
 for id in ids:
-  carac=''
-  url='https://store.steampowered.com/app/'+id
-  caracteristicas=scrapGlobal_text(url, True,'div', True, 'label')
-  categorias=scrapGlobal_text(url, True,'a', True, 'app_tag')
+  carac = ''
+  url = 'https://store.steampowered.com/app/' + id.split('/')[-1]
+  caracteristicas = scrapGlobal_text(url, True, 'div', True, 'label')
+  categoria= scrapSpecific_text(url, False, 'div', "details_block", 'a', False, 'app_tag')
 
-  categoriasPopulares.append(categorias)
-  for caracteristica in caracteristicas:  
+  categoriasPopulares.append(categoria)
+  for caracteristica in caracteristicas:
     #print(caracteristica)
-    carac=carac+' '+caracteristica
+    carac = carac + ' ' + caracteristica
   #print(carac)
-  if ('Online' in carac) or ('Multiplayer' in carac) or ('Anti-Cheat' in carac):
+  if ('Online' in carac) or ('Multiplayer' in carac) or ('Anti-Cheat'
+                                                         in carac):
     esMultijugador.append('Multijugador')
   else:
     esMultijugador.append('No multijugador')
 
-juegosPopulares={}
+juegosPopulares = {}
 
 for i in range(len(nombresJuegos)):
   if nombresJuegos[i] not in juegosPopulares:
     juegosPopulares[nombresJuegos[i]] = []
-  juegosPopulares[nombresJuegos[i]].append(ids[i])
+  juegosPopulares[nombresJuegos[i]].append(ids[i].split('/')[-1])
   juegosPopulares[nombresJuegos[i]].append(jugadoresPico[i])
   juegosPopulares[nombresJuegos[i]].append(preciosPopulares[i])
   juegosPopulares[nombresJuegos[i]].append(esMultijugador[i])
   juegosPopulares[nombresJuegos[i]].append(categoriasPopulares[i])
 
-
 print(juegosPopulares)
-
 
 ruta_archivo_csv = 'datosPluas.csv'
 with open(ruta_archivo_csv, 'w', newline='', encoding='utf-8') as archivo_csv:
   escritor_csv = csv.writer(archivo_csv, delimiter=';')
 
-  escritor_csv.writerow(['Nombre', 'ID', 'Jugadores pico diarios', 'Precio en $', 'Jugabilidad', 'Categorías'])
+  escritor_csv.writerow([
+      'Nombre de juego', 'ID', 'Jugadores pico diarios', 'Precio en $',
+      'Jugabilidad', 'Categorías'
+  ])
 
-  for datos in zip(nombresJuegos, ids, jugadoresPico, preciosPopulares, esMultijugador, categoriasPopulares):
-    datos = list(datos[:4]) + [','.join(map(str, sublist)) if isinstance(sublist, list) else sublist for sublist in datos[4:5]] + [','.join(map(str, datos[5]))]
-    escritor_csv.writerow(datos)
+  # Escribe los datos de las listas paralelas
+  for datos in zip(nombresJuegos, ids, jugadoresPico, preciosPopulares,
+    esMultijugador, categoriasPopulares):
+      escritor_csv.writerow(datos)
